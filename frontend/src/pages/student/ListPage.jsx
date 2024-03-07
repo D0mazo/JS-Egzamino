8366127ddfbe57c494d29af53c69075242863835
D0mazo <dominykas.simanavicius@proton.me>
2024-03-07 10:02:08 +0200

    skelbimai pasirodo fronte

--- frontend/src/pages/student/ListPage.jsx
+++ frontend/src/pages/student/ListPage.jsx
@@ -6,97 +6,100 @@ import toast from "react-hot-toast";
 import {useAuthContext} from "../../store/AuthCtxProvider.jsx";
 import {useMemo, useState} from "react";
 
-export default function StudentListPage() {
-    const [filterValue, setFilterValue] = useState('');
+export default function ListingPage() {
+  const [filterValue, setFilterValue] = useState('');
+  const [listingData, setListingData] = useApiData(`${baseApiUrl}skelbimai`);
+  const { isUserAdmin, isUserLoggedIn, token } = useAuthContext();
+  const navigate = useNavigate();
 
-    const [studentList, setStudentList] = useApiData(`${baseApiUrl}students`);
-
-    const {isUserAdmin, isUserLoggedIn, token} = useAuthContext();
-
-    const navigate = useNavigate();
-
-    const deleteStudent = async (studentId) => {
-        axios
-            .delete(`${baseApiUrl}students/${studentId}`, {
-                headers: {'Authorization': token}
-            })
-            .then((response) => {
-                navigate('/list-student');
-                toast.success(`Studentas ID: ${studentId} sėkmingai ištrintas!`);
-
-                const list = studentList.filter(student => student.id !== studentId); //only remove the selected row
-                setStudentList(list);
-            })
-            .catch((error) => {
-                toast.error(error.response.data.error);
-            })
+  const deleteListing = async (listingId) => {
+    try {
+      await axios.delete(`${baseApiUrl}skelbimai/${listingId}`, {
+        headers: { Authorization: token },
+      });
+      navigate('/listings');
+      toast.success(`Listing ID: ${listingId} successfully deleted!`);
+      const updatedListings = listingData.filter((listing) => listing.id !== listingId);
+      setListingData(updatedListings);
+    } catch (error) {
+      toast.error(error.response?.data?.error || 'Something went wrong');
     }
+  };
 
-    const filteredStudents = useMemo(() => {
-       return studentList.filter(student => student.firstname.toLowerCase().includes(filterValue.toLowerCase()) || student.lastname.toLowerCase().includes(filterValue.toLowerCase()) || student.email.toLowerCase().includes(filterValue.toLowerCase()))
-    }, [studentList, filterValue]);
-
-    const handleFilterChange = event => {
-        setFilterValue(event.target.value);
-    }
-
-    return (
-        <div className='container mx-auto p-4'>
-            <h1 className='text-4xl'>Studentu sarasas</h1>
-
-            <div className="mt-5">
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text"
-                    onChange={handleFilterChange}
-                    value={filterValue}
-                    placeholder="Pasieška"
-                />
-            </div>
-            <div className='mt-5'>
-                <table className="min-w-full table-auto">
-                    <thead className="bg-gray-600 text-white">
-                    <tr>
-                        <th className="px-4 py-2">ID</th>
-                        <th className="px-4 py-2">Vardas</th>
-                        <th className="px-4 py-2">Pavardė</th>
-                        <th className="px-4 py-2">El. paštas</th>
-                        <th className="px-4 py-2"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {filteredStudents.map((student) => (
-                        <tr key={student.id} className="bg-gray-200">
-                            <td className="border px-4 py-2">{student.id}</td>
-                            <td className="border px-4 py-2">{student.firstname}</td>
-                            <td className="border px-4 py-2">{student.lastname}</td>
-                            <td className="border px-4 py-2">{student.email}</td>
-                            <td className="border px-4 py-2">
-                                { isUserLoggedIn && (
-                                    <>
-                                        <Link
-                                            to={`/edit-student/${student.id}`}
-                                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                        >
-                                            Redaguoti
-                                        </Link>
-
-                                        { isUserAdmin && (
-                                            <button
-                                                className="bg-red-500 hover:bg-red-400 text-white font-bold ml-2 py-2 px-4 rounded"
-                                                onClick={() => deleteStudent(student.id)}
-                                            >
-                                                Ištrinti
-                                            </button>
-                                        )}
-                                    </>
-                                )}
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
+  const filteredListings = useMemo(() => {
+    return listingData.filter(
+      (listing) =>
+        listing.title.toLowerCase().includes(filterValue.toLowerCase()) ||
+        listing.category.toLowerCase().includes(filterValue.toLowerCase()) ||
+        listing.description.toLowerCase().includes(filterValue.toLowerCase())
     );
+  }, [listingData, filterValue]);
+
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value);
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl">Listings</h1>
+
+      <div className="mt-5">
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          onChange={handleFilterChange}
+          value={filterValue}
+          placeholder="Search"
+        />
+      </div>
+
+      <div className="mt-5">
+        <table className="min-w-full table-auto">
+          <thead className="bg-gray-600 text-white">
+            <tr>
+              <th className="px-4 py-2">ID</th>
+              <th className="px-4 py-2">Title</th>
+              <th className="px-4 py-2">Category</th>
+              <th className="px-4 py-2">Description</th>
+              <th className="px-4 py-2">Price</th>
+              <th className="px-4 py-2">URL</th>
+              <th className="px-4 py-2"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredListings.map((listing) => (
+              <tr key={listing.id} className="bg-gray-200">
+                <td className="border px-4 py-2">{listing.id}</td>
+                <td className="border px-4 py-2">{listing.title}</td>
+                <td className="border px-4 py-2">{listing.category}</td>
+                <td className="border px-4 py-2">{listing.description}</td>
+                <td className="border px-4 py-2">{listing.price}</td>
+                <td className="border px-4 py-2">{listing.url}</td>
+                <td className="border px-4 py-2">
+                  {isUserLoggedIn && (
+                    <>
+                      <Link
+                        to={`/edit-listing/${listing.id}`}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                      >
+                        Edit
+                      </Link>
+                      {isUserAdmin && (
+                        <button
+                          className="bg-red-500 hover:bg-red-400 text-white font-bold ml-2 py-2 px-4 rounded"
+                          onClick={() => deleteListing(listing.id)}
+                        >
+                          Delete
+                        </button>
+                      )}
+                    </>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
